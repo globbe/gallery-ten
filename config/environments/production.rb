270dcc3bbb712d68b031a896a01bed84331cddc2
Henrik Johansson <Henrik Johansson>
2012-02-02 01:57:09 -0800

    added bootstrap precomile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -17,7 +17,7 @@ Galleryten::Application.configure do
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = false
 
-  config.assets.precompile += %w(bootstrap/bootstrap.css bootstrap/bootstrap-responsive.css)
+  #config.assets.precompile += %w(bootstrap/bootstrap.css bootstrap/bootstrap-responsive.css)
 
   # Generate digests for assets URLs
   config.assets.digest = true
